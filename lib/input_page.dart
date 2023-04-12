f9b2a18ffabe0b964c0f91c6716b945e4a4bc7a4
Nimantha Karunanayake <nimanthakmp@gmail.com>
2023-04-12 23:38:52 +0530

    Create the Layouts and Refactor the Code

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,12 +16,59 @@ class _InputPageState extends State<InputPage> {
         centerTitle: true,
         title: const Text('BMI CALCULATOR'),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-            color: const Color(0xFF1D1E33),
-            borderRadius: BorderRadius.circular(10.0)),
-      )
+      body: Column(
+        children: [
+          //const SizedBox(height: 10,),
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
+                ),
+              ],
+            ),
+          ),
+
+          const Expanded(
+            child: ReusableCard(colour: Color(0xFF1D1E33),),
+          ),
+
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+  const ReusableCard({super.key, required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // height: 190,
+      // width: 185,
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
