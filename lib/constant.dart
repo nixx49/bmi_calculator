9274237031127de69765f69c9625ec03a845f020
Nimantha Karunanayake <nimanthakmp@gmail.com>
2023-04-22 00:27:32 +0530

    Adding const and setup the result page

--- lib/constant.dart
+++ lib/constant.dart
@@ -14,3 +14,22 @@ const kNumberTextStyle = TextStyle(
   fontSize: 50.0,
   fontWeight: FontWeight.w900,
 );
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+    fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold
+);
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0
+);
--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,4 +1,6 @@
+import 'package:bmi_calculator/reusable_code.dart';
 import 'package:flutter/material.dart';
+import 'constant.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({Key? key}) : super(key: key);
@@ -7,9 +9,40 @@ class ResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color(0xFF0A0E21),
+        centerTitle: true,
         title: const Text('BML CALCULATOR'),
       ),
-      body: const Text('HELLO'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(child: Container(
+            child: const Text('Your Result',
+              style: kTitleTextStyle,
+            ),
+          ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+               cardChild: Column(
+                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                 crossAxisAlignment: CrossAxisAlignment.center,
+                 children: const <Widget>[
+                   Text('Normal',
+                   style: kResultTextStyle,),
+                   Text('18.3',style: kBMITextStyle,
+                    ),
+                   Text('your BMI is quite low. please eat more and do exersice daily!', textAlign: TextAlign.center,style: kBodyTextStyle,)
+                 ],
+               ),
+              onPress: (){},
+            ),
+          )
+        ],
+      ),
     );
-  } 
+  }
 }
